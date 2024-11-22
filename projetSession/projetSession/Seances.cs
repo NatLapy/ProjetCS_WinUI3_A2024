244d1c490b5f1e8a35158853d3c43b6ef8c73818
NathLapy <164022199+NatLapy@users.noreply.github.com>
2024-11-22 13:00:32 -0500

    Création Séances

---
+++ projetSession/projetSession/Seances.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetSession
+{
+    internal class Seances
+    {
+        int idSceances = 0;
+        String dateOrganisation = "";
+        String heureOrganisation = "";
+        int nbPlaceDispo = 0;
+
+        public Seances(int idSceances, string dateOrganisation, string heureOrganisation, int nbPlaceDispo)
+        {
+            this.idSceances = idSceances;
+            this.dateOrganisation = dateOrganisation;
+            this.heureOrganisation = heureOrganisation;
+            this.nbPlaceDispo = nbPlaceDispo;
+        }
+
+        public int IdSceances
+        {
+            get
+            {
+                return idSceances;
+            }
+
+            set
+            {
+                idSceances = value;
+
+
+            }
+        }
+
+        public String DateOrganisation
+        {
+            get
+            {
+                return dateOrganisation;
+            }
+
+            set
+            {
+                dateOrganisation = value;
+
+
+            }
+        }
+
+        public String HeureOrganisation
+        {
+            get
+            {
+                return heureOrganisation;
+            }
+
+            set
+            {
+                heureOrganisation = value;
+
+
+            }
+        }
+
+        public int NbPlaceDispo
+        {
+            get
+            {
+                return nbPlaceDispo;
+            }
+
+            set
+            {
+                nbPlaceDispo = value;
+
+
+            }
+        }
+
+        public override string ToString()
+        {
+            return idSceances + " " + dateOrganisation + " " + heureOrganisation + " " + nbPlaceDispo;
+        }
+    }
+}
