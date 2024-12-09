7110ec66552d155fad7e7907aea87f2f5215681d
NathLapy <164022199+NatLapy@users.noreply.github.com>
2024-12-09 13:09:51 -0500

    Avancement Page statistique

--- projetSession/projetSession/Pages/PageStatistique.xaml.cs
+++ projetSession/projetSession/Pages/PageStatistique.xaml.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using projetSession.Classes;
+using projetSession.Singletons;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -26,6 +30,33 @@ namespace projetSession.Pages
         public PageStatistique()
         {
             this.InitializeComponent();
+
+            tb_nbTotalAdherent.Text = singletonBD.getInstance().getListeAdherent().Count.ToString();
+
+            tb_nbTotalActivite.Text = singletonBD.getInstance().getListe().Count.ToString();
+
+            ObservableCollection<Activites> listeActivite = singletonBD.getInstance().getListe();
+
+            if(listeActivite is not null)
+            {
+                StackPanel stk = new StackPanel();
+                TextBlock textBlock = new TextBlock();
+                TextBlock nombre = new TextBlock();
+
+
+                //exercice basket a compléter
+
+                stk.HorizontalAlignment = HorizontalAlignment.Stretch;
+                stk.BorderThickness = new Thickness(4.00);
+                stk.BorderBrush = new SolidColorBrush(Colors.Red);
+                stk.Padding = new Thickness(20);
+
+
+
+                stk.Children.Add(textBlock);
+                stk.Children.Add(nombre);
+                stk_nbAdherentParActivite.Children.Add(stk);
+            }
         }
     }
 }
